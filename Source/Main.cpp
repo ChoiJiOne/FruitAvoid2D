aa1b7bbcdbbdb81115f83531e9996d312c0420b1
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-10-02 09:15:02 +0900

    SDL2의 초기화/해제 코드 추가

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include <SDL2/SDL.h>
+
 int main(int argc, char* argv[])
 {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0)
+    {
+        std::cout << "failed to initialize SDL2" << std::endl;
+        return 0;
+    }
+
+    SDL_Quit();
     return 0;
 }
