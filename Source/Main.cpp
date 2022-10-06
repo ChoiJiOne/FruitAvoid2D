cdd38bcd8853e67d91e3cc8c7297b829190dba06
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-10-06 21:33:31 +0900

    main 함수 내의 리소스 초기화 부분을 함수로 분리

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,16 +1,11 @@
 #include "Player.h"
 #include "GameEngine.h"
 
-int main(int argc, char* argv[])
+void LoadResource()
 {
-    GameEngine::Init();
+	ResourceSystem& Resource = GameEngine::GetResourceSystem();
 
-    WindowSystem& Window = GameEngine::CreateWindowSystem("FruitAvoid2D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
-    RenderSystem& Renderer = GameEngine::CreateRenderSystem();
-    InputSystem& Input = GameEngine::CreateInputSystem();
-    ResourceSystem& Resource = GameEngine::CreateResourceSystem();
-
-    std::string ResourcePath = GameEngine::GetRootDirectory() + "\\Resource\\";
+	std::string ResourcePath = GameEngine::GetRootDirectory() + "\\Resource\\";
 	std::vector<std::string> TextureNames = {
 		"Banana",
 		"Coconut",
@@ -46,9 +41,21 @@ int main(int argc, char* argv[])
 		Resource.LoadTextureFromFile(Text::GetHash(TextureName), Text::Format("%stexture\\%s.png", ResourcePath.c_str(), TextureName.c_str()));
 	}
 
-    Texture& BGTexture = Resource.LoadTextureFromFile(Text::GetHash("Beach"), ResourcePath + "texture\\Beach.jpg");
-    Font& MonoFont = Resource.LoadFontFromFile(Text::GetHash("Mono"), ResourcePath + "font\\JetBrainsMono-Bold.ttf", 0x20, 0x7E, 32.0f);
-	
+	Resource.LoadTextureFromFile(Text::GetHash("Beach"), ResourcePath + "texture\\Beach.jpg");
+	Resource.LoadFontFromFile(Text::GetHash("Mono"), ResourcePath + "font\\JetBrainsMono-Bold.ttf", 0x20, 0x7E, 32.0f);
+}
+
+int main(int argc, char* argv[])
+{
+    GameEngine::Init();
+
+    WindowSystem& Window = GameEngine::CreateWindowSystem("FruitAvoid2D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
+    RenderSystem& Renderer = GameEngine::CreateRenderSystem();
+    InputSystem& Input = GameEngine::CreateInputSystem();
+    ResourceSystem& Resource = GameEngine::CreateResourceSystem();
+
+	LoadResource();
+
 	Player player(Vec2i(500, 650), 200.0f, 50, 50, Player::EColor::Blue);
 	Timer timer;
 
@@ -61,7 +68,7 @@ int main(int argc, char* argv[])
 
         Renderer.BeginFrame(Color::Black);
 
-        Renderer.DrawTexture2D(BGTexture, Vec2i(500, 400), 1000, 800);
+        Renderer.DrawTexture2D(Resource.GetTexture(Text::GetHash("Beach")), Vec2i(500, 400), 1000, 800);
 		player.Render(Renderer);
         
         Renderer.EndFrame();
