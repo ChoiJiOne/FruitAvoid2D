2f79ef817a0261920de8506348408e00d8345b94
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-10-28 20:46:43 +0900

    메인 함수에 윈도우 창을 생성 코드 추가

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -2,10 +2,17 @@
 
 int main(int argc, char* argv[])
 {
-	if (!SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+	if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS))
 	{
 		return -1;
 	}
+
+	SDL_Window* Window = SDL_CreateWindow("FruitAvoid2D", 100, 100, 1000, 800, SDL_WINDOW_SHOWN);
+
+	SDL_Delay(3000);
+
+	SDL_DestroyWindow(Window);
+	Window = nullptr;
 
 	SDL_Quit();
 	return 0;
