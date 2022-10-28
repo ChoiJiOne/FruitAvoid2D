27d788e001d28140e3625d392e0786b98bc486fe
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-10-28 21:17:45 +0900

    SDL2 확장 라이브러리들 초기화/해제 코드 추가

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,10 +1,21 @@
 #include "Window.h"
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 
 int main(int argc, char* argv[])
 {
-	CHECK((SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS) == 0), "failed to initialize SDL");
+	int32_t SDLFlags = SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_EVENTS;
+	int32_t ImageFlags = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP | IMG_INIT_JXL | IMG_INIT_AVIF;
+	int32_t SoundFlags = MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_MID | MIX_INIT_OPUS;
+
+	CHECK((SDL_Init(SDLFlags) == 0), "failed to initialize SDL");
+	CHECK((IMG_Init(ImageFlags) & ImageFlags), "failed to initialize SDL_image");
+	CHECK((TTF_Init() == 0), "failed to initialize SDL_ttf");
+	CHECK((Mix_Init(SoundFlags) & SoundFlags), "failed to initialize SDL_mixer");
+	CHECK((Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == 0), "failed to open audio");
 
 	Window window(
 		WindowConstructorParam{
@@ -41,6 +52,9 @@ int main(int argc, char* argv[])
 	SDL_DestroyRenderer(Renderer);
 	Renderer = nullptr;
 
+	Mix_Quit();
+	TTF_Quit();
+	IMG_Quit();
 	SDL_Quit();
 	return 0;
 }
