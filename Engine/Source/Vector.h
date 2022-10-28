5ee78c4ac2d788bf480871daa689d43488f4218a
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-10-28 21:26:37 +0900

    glm의 벡터 자료형을 재정의

---
+++ Engine/Source/Vector.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "glm/glm.hpp"
+
+
+/**
+ * glm의 벡터를 ToyEngine2D 에 맞게 재정의합니다.
+ */
+using Vec2i = glm::ivec2;
+using Vec2f = glm::vec2;
+using Vec3i = glm::ivec3;
+using Vec3f = glm::vec3;
+using Vec4i = glm::ivec4;
+using Vec4f = glm::vec4;
