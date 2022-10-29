7f2e3c22c922cfafb4138935f16dbe9c83c41395
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-10-29 15:23:56 +0900

    메인 함수에 ToyEngine 초기화/해제 코드 추가

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,33 +1,9 @@
-#include <iostream>
-#include <unordered_map>
-#include <ctime>
-
-#include "CommandLine.h"
-#include "CrashReport.h"
-#include "Window.h"
-#include "Text.h"
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
-
-void test()
-{
-	std::cout << "call test function" << std::endl;
-	int a = 10;
-	throw std::exception("");
-}
+#include "ToyEngine.h"
 
 int main(int argc, char* argv[])
 {
-	CommandLine::Parse();
-	CrashReport::Init();
+	ToyEngine::Init();
 
-	std::wcout << CommandLine::GetValue(L"-Dump") << std::endl;
-	std::wcout << CommandLine::GetValue(L"-Content") << std::endl;
-
-	test();
-
+	ToyEngine::Quit();
 	return 0;
 }
