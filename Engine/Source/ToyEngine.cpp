2289943a9cc5e81a74b3e29c12bd2240b353927b
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-11-02 12:46:12 +0900

    커맨드 라인 파싱과 크래시 덤프 초기화 코드 위치 수정

--- Engine/Source/ToyEngine.cpp
+++ Engine/Source/ToyEngine.cpp
@@ -2,6 +2,9 @@
 
 void ToyEngine::Init()
 {
+	CommandLine::Parse();
+	CrashReport::Init();
+
 	int32_t SDLFlags = SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_EVENTS;
 	int32_t ImageFlags = IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP | IMG_INIT_JXL | IMG_INIT_AVIF;
 	int32_t SoundFlags = MIX_INIT_FLAC | MIX_INIT_MOD | MIX_INIT_MP3 | MIX_INIT_OGG | MIX_INIT_MID | MIX_INIT_OPUS;
@@ -11,9 +14,6 @@ void ToyEngine::Init()
 	CHECK((TTF_Init() == 0), "failed to initialize SDL_ttf");
 	CHECK((Mix_Init(SoundFlags) & SoundFlags), "failed to initialize SDL_mixer");
 	CHECK((Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == 0), "failed to open audio");
-
-	CommandLine::Parse();
-	CrashReport::Init();
 
 #if defined(SHIPPING)
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
