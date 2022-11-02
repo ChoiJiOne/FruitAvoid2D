0316278be388b897730be00c0abcd0e229ae7392
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-11-02 21:06:21 +0900

    잘못된 Command Line Argument 수정

--- premake5.lua
+++ premake5.lua
@@ -107,8 +107,8 @@ workspace "FruitAvoid2D"
         links {"Engine"}
 
         debugargs {
-            "-Content=%{wks.location}/../Game/Content/",
-            "-Dump=%{wks.location}/../Dump/"
+            "-Content=%{wks.location}..\\Game\\Content\\",
+            "-Dump=%{wks.location}..\\Dump\\"
         }
 
         includedirs {
