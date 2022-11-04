2dd3ff2d8d95667d4299065db9f46daaf6c1fb45
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-11-04 16:07:14 +0900

    Font class 를 SDL_ttf 에서 stb_truetype 으로 변경

--- Engine/Source/Font.cpp
+++ Engine/Source/Font.cpp
@@ -1,62 +1,182 @@
 #include "Font.h"
-#include "Texture.h"
 #include "Graphics.h"
 
 // @third party code - BEGIN
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <stb/stb_rect_pack.h>
+#include <stb/stb_truetype.h>
 // @third party code - END
 
-Font::Font(const std::string& InPath, const int32_t& InSize)
-	: Size_(InSize)
+Font::Font(
+	Graphics& InGraphics,
+	const std::string& InPath,
+	int32_t InBeginCodePoint,
+	int32_t InEndCodePoint,
+	float InFontSize
+) : BeginCodePoint_(InBeginCodePoint),
+	EndCodePoint_(InEndCodePoint)
 {
-	Font_ = TTF_OpenFont(InPath.c_str(), Size_);
-	CHECK((Font_ != nullptr), "failed to create font resource");
+	std::vector<uint8_t> Buffer;
+	CHECK((LoadTrueTypeFontFromFile(InPath, Buffer)), "failed to load trye type font");
+
+	stbtt_fontinfo Info;
+	CHECK((stbtt_InitFont(
+		&Info,
+		reinterpret_cast<const unsigned char*>(&Buffer[0]),
+		stbtt_GetFontOffsetForIndex(reinterpret_cast<const unsigned char*>(&Buffer[0]), 0)
+	) != 0), "failed to initialize stb_truetype");
+
+	int32_t BitmapSize = 0;
+	std::shared_ptr<uint8_t[]> Bitmap = GenerateTextureAtlasBitmap(Buffer, BeginCodePoint_, EndCodePoint_, InFontSize, CharacterInfos_, BitmapSize);
+
+	TextureAtlas_ = CreateTextureAtlasFromBitmap(InGraphics, Bitmap, BitmapSize);
 }
 
 Font::~Font()
 {
-	if (Font_)
+	if (TextureAtlas_)
 	{
-		TTF_CloseFont(Font_);
-		Font_ = nullptr;
+		SDL_DestroyTexture(TextureAtlas_);
+		TextureAtlas_ = nullptr;
 	}
 }
 
-SDL_Surface* Font::CreateTextSurface(Graphics& InGraphics, const std::string& InText, const LinearColor& InColor)
+const CharacterInfo& Font::GetCharacterInfo(int32_t InCodePoint) const
 {
-	SDL_Color TextColor;
-	Color::ToR8G8B8A8(InColor, TextColor.r, TextColor.g, TextColor.b, TextColor.a);
-
-	return TTF_RenderText_Solid(Font_, InText.c_str(), TextColor);
+	CHECK(HasCodePointInRange(InCodePoint), "code point is out of range");
+	int32_t Index = InCodePoint - BeginCodePoint_;
+	return CharacterInfos_[Index];
 }
 
-SDL_Surface* Font::CreateTextSurface(Graphics& InGraphics, const std::wstring& InText, const LinearColor& InColor)
+bool Font::HasCodePointInRange(int32_t InCodePoint) const
 {
-	SDL_Color TextColor;
-	Color::ToR8G8B8A8(InColor, TextColor.r, TextColor.g, TextColor.b, TextColor.a);
-
-	return TTF_RenderUNICODE_Solid(Font_, reinterpret_cast<const uint16_t*>(InText.c_str()), TextColor);
+	return (BeginCodePoint_ <= InCodePoint) && (InCodePoint <= EndCodePoint_);
 }
 
-SDL_Texture* Font::CreateTextTexture(Graphics& InGraphics, const std::string& InText, const LinearColor& InColor)
+void Font::MeasureText(const std::wstring& InText, int32_t& OutWidth, int32_t& OutHeight) const
 {
-	SDL_Surface* TextSurface = CreateTextSurface(InGraphics, InText, InColor);
-	SDL_Texture* TextTexture = Texture::CreateTextureFromSurface(InGraphics, TextSurface);
+	int32_t TextHeight = -1;
+	int32_t TextWidth = 0;
 
-	SDL_FreeSurface(TextSurface);
-	TextSurface = nullptr;
+	for (const auto& Unicode : InText)
+	{
+		const CharacterInfo& UnicodeInfo = GetCharacterInfo(static_cast<int32_t>(Unicode));
 
-	return TextTexture;
+		int32_t CurrWidth = static_cast<int32_t>(UnicodeInfo.XAdvance);
+		int32_t CurrHeight = UnicodeInfo.Position1.y - UnicodeInfo.Position0.y;
+
+		TextWidth += CurrWidth;
+
+		if (CurrHeight > TextHeight)
+		{
+			TextHeight = CurrHeight;
+		}
+	}
+
+	OutWidth = TextWidth;
+	OutHeight = TextHeight;
 }
 
-SDL_Texture* Font::CreateTextTexture(Graphics& InGraphics, const std::wstring& InText, const LinearColor& InColor)
+bool Font::LoadTrueTypeFontFromFile(const std::string& InPath, std::vector<uint8_t>& OutBuffer)
 {
-	SDL_Surface* TextSurface = CreateTextSurface(InGraphics, InText, InColor);
-	SDL_Texture* TextTexture = Texture::CreateTextureFromSurface(InGraphics, TextSurface);
+	OutBuffer.resize(1 << 25);
 
-	SDL_FreeSurface(TextSurface);
-	TextSurface = nullptr;
+	FILE* FontFile = nullptr;
+	fopen_s(&FontFile, InPath.c_str(), "rb");
 
-	return TextTexture;
+	if (!FontFile) return false;
+
+	fread(&OutBuffer[0], 1, 1 << 25, FontFile);
+	fclose(FontFile);
+
+	return true;
+}
+
+std::shared_ptr<uint8_t[]> Font::GenerateTextureAtlasBitmap(
+	const std::vector<uint8_t>& InBuffer, 
+	int32_t InBeginCodePoint, 
+	int32_t InEndCodePoint, 
+	float InFontSize, 
+	std::vector<CharacterInfo>& OutCharacterInfos, 
+	int32_t& OutSize
+)
+{
+	std::vector<stbtt_packedchar> Packedchars(InEndCodePoint - InBeginCodePoint + 1);
+	OutCharacterInfos.resize(InEndCodePoint - InBeginCodePoint + 1);
+
+	int32_t Success = 0;
+	stbtt_pack_context PackContext;
+	std::shared_ptr<uint8_t[]> Bitmap = nullptr;
+
+	for (int32_t Size = 16; Size < 8192; Size *= 2)
+	{
+		Bitmap = std::make_unique<uint8_t[]>(Size * Size);
+		Success = stbtt_PackBegin(&PackContext, Bitmap.get(), Size, Size, 0, 1, nullptr);
+		stbtt_PackSetOversampling(&PackContext, 1, 1);
+
+		Success = stbtt_PackFontRange(
+			&PackContext,
+			reinterpret_cast<const unsigned char*>(&InBuffer[0]),
+			0,
+			InFontSize,
+			InBeginCodePoint,
+			static_cast<int>(Packedchars.size()),
+			&Packedchars[0]
+		);
+
+		if (Success)
+		{
+			stbtt_PackEnd(&PackContext);
+			OutSize = Size;
+			break;
+		}
+		else
+		{
+			stbtt_PackEnd(&PackContext);
+			Bitmap.reset();
+		}
+	}
+
+	for (std::size_t Index = 0; Index < Packedchars.size(); ++Index)
+	{
+		OutCharacterInfos[Index].CodePoint = Index + InBeginCodePoint;
+
+		OutCharacterInfos[Index].Position0 = Vec2i(Packedchars[Index].x0, Packedchars[Index].y0);
+		OutCharacterInfos[Index].Position1 = Vec2i(Packedchars[Index].x1, Packedchars[Index].y1);
+
+		OutCharacterInfos[Index].XOffset = Packedchars[Index].xoff;
+		OutCharacterInfos[Index].YOffset = Packedchars[Index].yoff;
+
+		OutCharacterInfos[Index].XOffset2 = Packedchars[Index].xoff2;
+		OutCharacterInfos[Index].YOffset2 = Packedchars[Index].yoff2;
+
+		OutCharacterInfos[Index].XAdvance = Packedchars[Index].xadvance;
+	}
+
+	return Bitmap;
+}
+
+SDL_Texture* Font::CreateTextureAtlasFromBitmap(Graphics& InGraphics, const std::shared_ptr<uint8_t[]>& InBitmap, const int32_t InSize)
+{
+	auto Pixels = std::make_unique<uint32_t[]>(InSize * InSize);
+
+	SDL_Texture* TextureAtlas = SDL_CreateTexture(InGraphics.GetRenderer(), SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, InSize, InSize);
+	CHECK((TextureAtlas != nullptr), "failed to create texture altas");
+
+	CHECK((SDL_SetTextureBlendMode(TextureAtlas, SDL_BLENDMODE_BLEND) == 0), "failed to set blend mode in texture atlas");
+
+	SDL_PixelFormat* Format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA32);
+	CHECK((Format != nullptr), "failed to alloc format");
+
+	for (int32_t index = 0; index < InSize * InSize; ++index)
+	{
+		Pixels[index] = SDL_MapRGBA(Format, 0xFF, 0xFF, 0xFF, InBitmap[index]);
+	}
+
+	CHECK((SDL_UpdateTexture(TextureAtlas, nullptr, Pixels.get(), InSize * sizeof(uint32_t)) == 0), "failed to update texture atlas");
+
+	SDL_FreeFormat(Format);
+	Format = nullptr;
+
+	return TextureAtlas;
 }
