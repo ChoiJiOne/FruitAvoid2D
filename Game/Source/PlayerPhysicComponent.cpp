39cc0a5183375ce6e7f073b0749a1eadb4a3d9d3
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-11-09 09:47:23 +0900

    플레이어가 월드 밖으로 나가지 못하는 코드 추가

--- Game/Source/PlayerPhysicComponent.cpp
+++ Game/Source/PlayerPhysicComponent.cpp
@@ -1,6 +1,7 @@
 #include "PlayerPhysicComponent.h"
 #include "Body.h"
 #include "GameObject.h"
+#include "MathUtils.h"
 #include "World.h"
 
 void PlayerPhysicComponent::Tick(GameObject& InObject, World& InWorld, float InDeltaSeconds)
@@ -9,6 +10,7 @@ void PlayerPhysicComponent::Tick(GameObject& InObject, World& InWorld, float InD
 	Vec2f Position = ObjectBody.GetCenter();
 
 	Position.x += InDeltaSeconds * ObjectBody.GetVelocity() * static_cast<float>(cos(ObjectBody.GetRotate()));
+	Position.x = MathUtils::Clamp<float>(Position.x, 0.0f, static_cast<float>(InWorld.GetWidth()));
 
 	ObjectBody.SetCenter(Position);
 }
