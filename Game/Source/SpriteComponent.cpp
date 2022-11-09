24dd033499fe8423a71bbcdb90dadda65240a99e
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-11-09 11:27:58 +0900

    텍스처의 영역을 그리는 코드 추가 (테스트 종료 후 삭제 예정)

--- Game/Source/SpriteComponent.cpp
+++ Game/Source/SpriteComponent.cpp
@@ -14,6 +14,32 @@ void SpriteComponent::Tick(GameObject& InObject, Graphics& InGraphics)
 		static_cast<int32_t>(ObjectBody.GetCenter().y + 0.5f)
 	);
 
+	const std::array<Vec2f, 4>& Positions = ObjectBody.GetBoundingPositions();
+
+	InGraphics.DrawLine2D(
+		Vec2i(static_cast<int32_t>(Positions[0].x + 0.5f), static_cast<int32_t>(Positions[0].y + 0.5f)),
+		Vec2i(static_cast<int32_t>(Positions[1].x + 0.5f), static_cast<int32_t>(Positions[1].y + 0.5f)),
+		ColorUtils::Black
+	);
+
+	InGraphics.DrawLine2D(
+		Vec2i(static_cast<int32_t>(Positions[1].x + 0.5f), static_cast<int32_t>(Positions[1].y + 0.5f)),
+		Vec2i(static_cast<int32_t>(Positions[3].x + 0.5f), static_cast<int32_t>(Positions[3].y + 0.5f)),
+		ColorUtils::Black
+	);
+
+	InGraphics.DrawLine2D(
+		Vec2i(static_cast<int32_t>(Positions[3].x + 0.5f), static_cast<int32_t>(Positions[3].y + 0.5f)),
+		Vec2i(static_cast<int32_t>(Positions[2].x + 0.5f), static_cast<int32_t>(Positions[2].y + 0.5f)),
+		ColorUtils::Black
+	);
+
+	InGraphics.DrawLine2D(
+		Vec2i(static_cast<int32_t>(Positions[2].x + 0.5f), static_cast<int32_t>(Positions[2].y + 0.5f)),
+		Vec2i(static_cast<int32_t>(Positions[0].x + 0.5f), static_cast<int32_t>(Positions[0].y + 0.5f)),
+		ColorUtils::Black
+	);
+
 	InGraphics.DrawTexture2D(
 		ContentUtils::GetTexture(SpriteKey_),
 		Position,
